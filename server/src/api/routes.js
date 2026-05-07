18eba911d1dcf19d40df998426d52cf06366b1c7
Bogas87 <KRZYSIEKARGOL@GMAIL.COM>
2026-05-07 13:17:48 +0200

    remove interview routes temp

--- server/src/api/routes.js
+++ server/src/api/routes.js
@@ -3,7 +3,6 @@
 const { Router } = require("express");
 const analyzeController = require("./analyze.controller.js");
 const stripeController = require("./stripe.controller.js");
-const interviewController = require("./interview.controller.js");
 
 const router = Router();
 
@@ -30,10 +29,6 @@ router.post("/analyze/checkpoint", analyzeController.generateCheckpoint);
 
 router.post("/create-checkout", stripeController.createCheckout);
 router.post("/stripe/checkout", stripeController.createCheckout);
-
-router.post("/interview/start", interviewController.startInterview);
-router.post("/interview/next", interviewController.nextQuestion);
-router.post("/interview/finish", interviewController.finishInterview);
 
 router.get("/report/signed", analyzeController.getSignedReport);
 router.get("/report/:token", analyzeController.getReport);
